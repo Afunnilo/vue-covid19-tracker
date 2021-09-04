7102fe7a9a133bce3caa6a7e9feee04f88844566
Afunnilo <57325456+Afunnilo@users.noreply.github.com>
2021-09-04 04:10:39 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="text-center bg-purple-700 text-white p-4 mb-10">
     <div class="text-3xl md:text-5xl font-bold mb-3"><i class = "fa fa-virus"></i>Covid-19 Tracker</div>
-    <p> API from <a class="text-300 plum" href="https://covid19api.com" target="_blank">covid10api.com</a></p>
+    <p> API from <a class="text-300 plum" href="https://covid19api.com" target="_blank">covid19api.com</a></p>
   </header>
 </template>
 
