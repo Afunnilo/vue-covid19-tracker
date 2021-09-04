2be3fceaef5ceaaea72a9a888eece8f20def7123
Afunnilo <57325456+Afunnilo@users.noreply.github.com>
2021-09-04 04:36:53 +0100

    Update CountrySelect.vue

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,7 +1,7 @@
 <template>
    <select
     v-model="selected" 
-    class="form-select mt-1 block w-full border p-3 pb-3 rounded"
+    class="form-select mt-1 bg-purple-100 block w-full border p-3 pb-3 rounded"
      @change="onChange()"
     >
        <option value="0">Select Country</option>
